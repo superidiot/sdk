145dd1121f92a8dcf0fbbc0ed946d597f68ed372
Super Idiot <superidiot1337@gmail.com>
2012-09-27 23:21:01 +0200

    reader and writer function seems to work

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+struct f {
+  int n;
+  int ns[9];
+  };
+
+struct s {
+  struct f a[9][9];
+} sudoku;
+
+void reader(FILE *fp){
+  int i,j,c;
+  for (i = 0; i < 9; i++){
+    for (j = 0; j < 9; j++){
+      fscanf(fp, "%1d", &c);
+      sudoku.a[i][j].n = c;
+    }
+  }
+}
+
+void printer (){
+  int i,j;
+  for (i = 0; i < 9; i++){
+    if (i % 3 == 0){
+      printf("%s","+-----------+\n");
+    }
+    for (j = 0; j < 9; j++){
+      if (j % 3 == 0){
+	printf("%s","|");
+      }
+      printf("%1d", sudoku.a[i][j].n);
+    }
+    printf("%1s","|\n");
+  }
+  printf("%s","+-----------+\n");
+}
+
+main (){
+  FILE *fp = fopen("sudoku.txt","r");
+  reader(fp);
+  fclose(fp);
+  printer();
+}
