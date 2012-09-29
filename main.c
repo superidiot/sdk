a6725757ff24498f300da69dd733acf9461d2651
Super Idiot <superidiot1337@gmail.com>
2012-09-30 01:13:15 +0200

    added support function to solver and some functions that operate on the support

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "sudoku.h"
 
 main (){
-
   struct s *sp = &sudoku;
  
   FILE *fp = fopen("sudoku.txt","r");
   reader(fp,sp);
   fclose(fp);
+  solver(sp);
   printer(sp);
 }
--- sudoku.h
+++ sudoku.h
@@ -5,7 +5,7 @@ struct f {
 
 struct s {
   struct f a[9][9];
-}sudoku;
+} sudoku;
 
 void reader(FILE *fd, struct s *sp);
 void printer (struct s *sp);
