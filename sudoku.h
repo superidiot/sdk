56fda93fe4b6bd863b1b81427a3c3dfc9a86e306
superidiot <superidiot@riker.(none)>
2012-10-12 21:49:49 +0200

    played around. forgot what i changed...

--- sudoku.h
+++ sudoku.h
@@ -1,13 +1,26 @@
 struct f {
   int n;
-  int ns[10];
+  int ns;
 };
 
 struct s {
-  struct f a[9][9];
-} sudoku;
+  struct f **sudoku_array;
+};
 
-void reader(FILE *fd, struct s *sp);
+enum numbers { ONE = 1,
+	       TWO = 1 << 1,
+	       THREE = 1 << 2,
+	       FOUR = 1 << 3,
+	       FIVE = 1 << 4,
+	       SIX = 1 << 5,
+	       SEVEN = 1 << 6,
+	       EIGHT = 1 << 7,
+	       NINE = 1 << 8};
+
+struct s* reader(FILE *fd, struct s *sp);
+ 
 void printer_cli (struct s *sp);
+/*
 void printer_ps (struct s *sp, const char *filename, int offsetX, int offsetY, const char *mode);
 void solver (struct s *sp);
+*/
