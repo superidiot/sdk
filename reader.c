59193875d2cb6f46f0fa91d96a5174b545c8be3d
Super Idiot <asdf@bsdf.org>
2012-10-13 13:46:46 +0200

    changed print_bin, need to do some pointer experiments again

--- reader.c
+++ reader.c
@@ -9,7 +9,13 @@ struct s* reader(FILE *fp){
     for (j = 0; j < 9; j++){
       struct f *field = (struct f *) malloc (sizeof(struct f));
       fscanf(fp, "%1d", &(field->n));
-      field->ns = ONE | TWO | THREE | FOUR | FIVE | SIX | SEVEN | EIGHT | NINE;
+      if (field->n == 0){
+	field->ns = ONE | TWO | THREE | FOUR | FIVE | SIX | SEVEN | EIGHT | NINE;
+      }
+      else {
+	field->ns = 0;
+      }
+	
       sudoku.a[i][j] = field;
     }
   }
