ac7838df0cd39e31b0f96adcd15bc52d49d312e6
Super Idiot <superidiot1337@gmail.com>
2013-04-30 20:54:34 +0200

    add macro to access sudoku as vector instead of matrix

--- reader.c
+++ reader.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sudoku.h"
+/* http://en.wikipedia.org/wiki/Row-major_order */
+
+#define MATRIX_ROW_MAJOR_IDX(A, ncol, row, col) A[row * ncol + col] /* offset = row*NUMCOLS + column. */
+#define MATRIX_COL_MAJOR_IDX(A, nrow, row, col) A[row + col * nrow] /* offset = row + column*NUMROWS. */
 
 struct s* reader(FILE *fp)
 {
