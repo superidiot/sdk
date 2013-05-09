413f1429303839ca7d7cc344aff9717531286d11
Super Idiot <superidiot1337@gmail.com>
2013-05-09 13:50:04 +0200

    add name of function to the debug macro

--- dbg.h
+++ dbg.h
@@ -8,8 +8,8 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", \
-			      __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "DEBUG %s[%s]:%d: " M "\n", \
+			      __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
@@ -22,6 +22,7 @@
 	  M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 #define log_info(M, ...)						\
   fprintf(stderr, "[INFO] (%s,%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
 #define check(A, M, ...) if (!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error;}
 
 #define sentinel(M, ...) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
